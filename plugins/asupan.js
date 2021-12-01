06cc75f1b92ccb2167f244df686a636d643b8914
Wahyu Anshar <77173960+Wahyudianshar@users.noreply.github.com>
2021-12-02 02:25:08 +0800

    Update and rename asupan.js to _antibadwords.js

--- plugins/asupan.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, API('amel', '/asupan', {}, 'apikey'), 'asupan.mp4', '© melcanz x ariffb')
-}
-handler.help = ['asupan']
-handler.tags = ['fun']
-handler.command = /^(asupan)$/i
-
-module.exports = handler
